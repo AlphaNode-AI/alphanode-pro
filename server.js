81ed0215c32538c4003fbd284acd9f04c1601b62
AlphaNode-AI <izharxahmad@gmail.com>
2026-05-08 12:16:48 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const cors = require('cors');
+const WebSocket = require('ws');
+
 const app = express();
 
-app.get('/', (req,res)=>{
-  res.json({status:'AlphaNode backend running'});
+app.use(cors());
+
+let latestPrice = null;
+
+const binanceWS = new WebSocket(
+  'wss://stream.binance.com:9443/ws/btcusdt@ticker'
+);
+
+binanceWS.on('message', (data) => {
+  const ticker = JSON.parse(data);
+
+  latestPrice = {
+    symbol: ticker.s,
+    price: ticker.c
+  };
+
+  console.log(latestPrice);
 });
 
-app.listen(3000, ()=>{
-  console.log('Backend running on port 3000');
+app.get('/', (req, res) => {
+  res.json({
+    status: 'AlphaNode Backend Running',
+    live: latestPrice
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
